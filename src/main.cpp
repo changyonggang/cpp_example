52f930452b66826e04e6c2fe32f0aa971f158d50
changyonggang <changyonggang@kingsoft.com>
2021-11-12 21:12:07 +0800

    refactor: refactor code

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "mult_thread/mult_thread.cpp"
+#include "utils/static_var.cpp"
+
+using namespace std;
+
+int main() {
+    thread_run();
+
+    run_thread_id();
+    return 0;
+}
+
+
+
---
+++ src/utils/static_var.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 常永刚 on 2021/11/12.
+//
+#include <iostream>
+
+int ThreadId(){
+    static int scheduler_id = 0;
+    static int zookeeper_id = 0;
+    static int service_id = 0;
+
+    printf("scheduler_id : %d\n", scheduler_id++);
+    printf("zookeeper_id : %d\n", zookeeper_id++);
+    printf("service_id : %d\n", service_id++);
+}
+
+void run_thread_id() {
+    std::cout << "Hello, World!" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        printf("loop start ...");
+        ThreadId();
+    }
+}
